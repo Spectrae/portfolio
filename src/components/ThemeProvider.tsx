b643d5ba48ee441c92c721aac17c65683d9f0f30
Spectrae <rick.dev.79@gmail.com>
2025-11-14 15:38:00 +0530

    fix: remove invalid props from ThemeProvider usage

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -2,18 +2,15 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode } from "react";
 
-export function ThemeProvider({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export function ThemeProvider({ children }: { children: ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
       defaultTheme="system"
-      enableSystem={true}
-      disableTransitionOnChange={true}
+      enableSystem
+      disableTransitionOnChange
     >
       {children}
     </NextThemesProvider>
